7c390e3bfdc0915d9d79ac85a0286070c1c2f0fc
Markus Dietrich <mdi1984@gmail.com>
2016-01-25 22:30:09 +0100

    get top level directories - used as submission folders

--- MossGuru/MossGuru.Core/Util/FileUtils.cs
+++ MossGuru/MossGuru.Core/Util/FileUtils.cs
@@ -7,6 +7,17 @@ namespace MossGuru.Core.Util
 {
   public static class FileUtils
   {
+    public static IEnumerable<DirectoryInfo> GetTopLevelDirectories(string path)
+    {
+      var dirInfo = new DirectoryInfo(path);
+      if (!dirInfo.Exists)
+      {
+        throw new DirectoryNotFoundException();
+      }
+
+      return dirInfo.GetDirectories("*", SearchOption.TopDirectoryOnly);
+    }
+
     public static IEnumerable<FileInfo> GetFilesFromDir(string path, string extension, string[] ignoreFolders = null)
     {
       var dirInfo = new DirectoryInfo(path);
