27120da5472602a142970adeff9323499810e859
Markus Dietrich <mdi1984@gmail.com>
2016-01-28 22:36:04 +0100

    added validation viewmodel baseclass for easier IDataErrorInfo handling

---
+++ MossGuru/MossGuru.UI/Validation/ValidationViewModelBase.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using GalaSoft.MvvmLight;
+
+namespace MossGuru.UI.Validation
+{
+  public abstract class ValidationViewModelBase : ViewModelBase, IDataErrorInfo
+  {
+    private readonly Dictionary<string, object> _values = new Dictionary<string, object>();
+
+    public new bool Set<T>(ref T field, T newValue = default(T), bool broadcast = false, [CallerMemberName] string propertyName = null)
+    {
+      _values[propertyName] = newValue;
+      return base.Set(ref field, newValue, broadcast, propertyName);
+    }
+
+    protected virtual string OnValidate(string propertyName)
+    {
+      if (string.IsNullOrEmpty(propertyName))
+      {
+        throw new ArgumentException("Invalid property name", propertyName);
+      }
+
+      string error = string.Empty;
+      var value = GetValue(propertyName);
+      var results = new List<System.ComponentModel.DataAnnotations.ValidationResult>(1);
+      var result = Validator.TryValidateProperty(
+          value,
+          new ValidationContext(this, null, null)
+          {
+            MemberName = propertyName
+          },
+          results);
+
+      if (!result)
+      {
+        var validationResult = results.First();
+        error = validationResult.ErrorMessage;
+      }
+
+      return error;
+    }
+
+    string IDataErrorInfo.Error
+    {
+      get
+      {
+        throw new NotSupportedException("IDataErrorInfo.Error is not supported, use IDataErrorInfo.this[propertyName] instead.");
+      }
+    }
+
+    string IDataErrorInfo.this[string propertyName]
+    {
+      get
+      {
+        return OnValidate(propertyName);
+      }
+    }
+
+    private object GetValue(string propertyName)
+    {
+      object value;
+      if (!_values.TryGetValue(propertyName, out value))
+      {
+        var propertyDescriptor = TypeDescriptor.GetProperties(GetType()).Find(propertyName, false);
+        if (propertyDescriptor == null)
+        {
+          throw new ArgumentException("Invalid property name", propertyName);
+        }
+
+        value = propertyDescriptor.GetValue(this);
+        _values.Add(propertyName, value);
+      }
+
+      return value;
+    }
+  }
+}
